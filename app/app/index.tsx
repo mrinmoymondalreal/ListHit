aac29c7be1959fe304eb2831677d226c3b8b7958
Mrinmoy Mondal <mrinmoymondalreal+github@gmail.com>
2025-02-02 00:49:35 +0530

    removed unwanted imports

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,8 +1,5 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-import { View } from "lucide-react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "~/components/ui/text";
 
 export default function Page() {
   const { isSignedIn } = useAuth();
